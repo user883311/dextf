3ae22a26eeab8729121cacb8cf7a0a0ae317c88f
user883311 <tarik.chardon@gmail.com>
2018-04-08 12:17:57 +0800

    Fixed anchors issue.
    Fixed issue with the navbar hiding initial content when jumping to an in-page anchor.

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ Source: ZimSystem at https://stackoverflow.com/a/42401686/2168164 */
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+// Makes sure that the navbar does not hides initial content when jumping to an in-page anchor. 
+// Source: https://github.com/twbs/bootstrap/issues/1768#issuecomment-6151214 
+window.addEventListener("hashchange", function() { scrollBy(0, -300) });
